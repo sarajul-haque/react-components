c60c368dbc7672c6e9ab3000dcf5a9e862c95980
sarajul <sarajulmd5@gmail.com>
2023-03-10 19:16:17 +0530

    Remove extra spaces and add className

--- marketing/src/sections/navigation/AlloyNav/AlloyNav.jsx
+++ marketing/src/sections/navigation/AlloyNav/AlloyNav.jsx
@@ -6,20 +6,18 @@ const AlloyNav = () => {
     return (
 
        <section>
-         <nav>
+         <nav className="navbar">
             <div>
                 <h1>alloy</h1>
                 <ul>
                     <a>Products</a> <select></select>
                 </ul>
-
                 <ul>
                     <a>Integrations</a>
                 </ul>
                 <ul>
                     <a>Recipes</a>
                 </ul>
-
                 <ul>
                     <a>Resources</a> <select></select>
                 </ul>
@@ -28,24 +26,16 @@ const AlloyNav = () => {
                 <ul>
                     <a>Book Demo</a>
                 </ul>
-
                 <ul>
                     <a>Login</a>
                 </ul>
-
                 <ul>
                     <a>Try it free</a> <select></select>
                 </ul>
-
             </div>
         </nav>
        </section>
-        
-       
-
     )
-
-
 }
 
 export default AlloyNav;
