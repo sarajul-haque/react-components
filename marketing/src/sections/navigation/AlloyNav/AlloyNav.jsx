8b5627f081c1487d8b574d59490f6fa39bca81b4
sarajul <sarajulmd5@gmail.com>
2023-03-16 18:36:12 +0530

    i have removed hover scale effect

--- marketing/src/sections/navigation/AlloyNav/AlloyNav.jsx
+++ marketing/src/sections/navigation/AlloyNav/AlloyNav.jsx
@@ -61,7 +61,7 @@ const AlloyNav = () => {
             <nav>
                 <div className="alloy-nav">
                     <img id="alloy-logo" src='/img/sections/hero/HeadStream/alloyLogos.png' alt='' height={30} />
-                    <a className="alloy-title" href="">alloy</a>
+                    <a className="alloy-title" href=""><span>alloy</span></a>
                     <a className="menu" href="">
                         <div style={{ display: "inline-block" }} onClick={(e) => {
                             e.preventDefault();
@@ -74,14 +74,14 @@ const AlloyNav = () => {
 
 
                     </a>
-                    <a href="">Integrations</a>
-                    <a href="">Recipes</a>
-                    <a href="">Resources</a>
+                    <a href=""><span>Integrations</span></a>
+                    <a href=""><span>Recipes</span></a>
+                    <a href=""><span>Resources</span></a>
 
                 </div>
                 <div className="alloy-right-nav">
-                    <a href="">Book Demo</a>
-                    <a href="">Login</a>
+                    <a href=""><span>Book Demo</span></a>
+                    <a href=""><span>Login</span></a>
                     <button>Try it free</button>
                 </div>
             </nav>
