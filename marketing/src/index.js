6827abb190f73c096e8bb0760c4f8ac7c9aeaf91
sarajul <msarajul72@gmail.com>
2023-03-22 14:17:20 +0530

    add truework nav

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import BlueSimple from "./sections/navigation/BlueSimple/BlueSimple";
+import TrueworkNav from "./sections/navigation/TrueworkNav/TrueWork";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BlueSimple />
+    <TrueworkNav />
   </React.StrictMode>
 );
 
